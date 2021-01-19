e02e246454e3b1c904e2a764b23ca12e11b09a23
Howard20181 <40033067+Howard20181@users.noreply.github.com>
2021-01-20 00:03:36 +0800

    Detect file location directly

---
+++ customize.sh
@@ -0,0 +1,18 @@
+if [[ -f "/system/product/etc/permissions/services.cn.google.xml" ]]; then
+    PERMISSION_PATH="${MODPATH}/system/product/etc/permissions"
+elif [[ -f "/system/etc/permissions/services.cn.google.xml" ]]; then
+    PERMISSION_PATH="${MODPATH}/system/etc/permissions"
+else
+    abort "services.cn.google.xml Not found!"
+fi
+
+ui_print "--PATH ${PERMISSION_PATH}"
+mkdir -p "${PERMISSION_PATH}"
+
+cat >"${PERMISSION_PATH}/services.cn.google.xml" <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<!-- This is the standard set of features for devices that support the CN GMSCore. -->
+<permissions>
+    <feature name="com.google.android.feature.services_updater" />
+</permissions>
+EOF
