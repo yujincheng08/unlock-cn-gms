4f0297d27e400eb7b70750950e77962b9dfc6b63
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2021-06-15 20:50:14 +0800

    直接检测文件地址&防止Play商店应用列表消失 (#2)
    * Detect file location directly
    
    * Prevent the Play Store from getting a list of installed apps

--- customize.sh
+++ customize.sh
@@ -1,17 +1,16 @@
-if [[ ${API} -ge 29 ]]; then
+if [ -f "/system/product/etc/permissions/services.cn.google.xml" ]; then
     PERMISSION_PATH="${MODPATH}/system/product/etc/permissions"
-else
+elif [ -f "/system/etc/permissions/services.cn.google.xml" ]; then
     PERMISSION_PATH="${MODPATH}/system/etc/permissions"
+else
+    abort "services.cn.google.xml Not found!"
 fi
 
 ui_print "--API ${API}"
 ui_print "--PATH ${PERMISSION_PATH}"
 mkdir -p "${PERMISSION_PATH}"
 
-cat > "${PERMISSION_PATH}/services.cn.google.xml" <<EOF
+cat >"${PERMISSION_PATH}/services.cn.google.xml" <<EOF
 <?xml version="1.0" encoding="utf-8"?>
 <!-- This is the standard set of features for devices that support the CN GMSCore. -->
-<permissions>
-    <feature name="com.google.android.feature.services_updater" />
-</permissions>
 EOF
